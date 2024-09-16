b61fb681b5a2df0f2f3b7344c1a02f454db4dde4
Eyub-Abdi <ayubabdiy@gmail.com>
2024-09-16 14:46:40 +0300

    Converted Natours to React App

---
+++ components/header/Header.jsx
@@ -0,0 +1,21 @@
+import './header.scss'
+export default function Header() {
+  return (
+    <>
+      <header className="header">
+        <div className="header__logo-box">
+          <img src="./img/logo-white.png" alt="Logo" className="header__logo" />
+        </div>
+        <div className="header__text-box">
+          <h1 className="heading-primary">
+            <span className="heading-primary--main">ZANZIBAR ISLAND</span>
+            <span className="heading-primary--sub">Is where life happens</span>
+          </h1>
+          <a href="#" className="btn btn--white btn--animated">
+            Discover our tours
+          </a>
+        </div>
+      </header>
+    </>
+  )
+}
