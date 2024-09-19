9c95db70f98007fd7182bd77fe53aed8a391474e
Eyub-Abdi <ayubabdiy@gmail.com>
2024-09-19 23:46:05 +0300

    Keep previos data persit

--- src/components/SectionBook.jsx
+++ src/components/SectionBook.jsx
@@ -2,12 +2,11 @@ import { useNavigate } from 'react-router-dom'
 import Joi from 'joi'
 import { useState } from 'react'
 import countries from '../countries'
-// CONTEXT
-import BookingContext from '../BookingContext'
-import FlashMassege from './FlashMassege'
+// COMPONENTS
+import FlashMassage from './FlashMassage'
 
 function SectionBook() {
-  const [guestData, setFullName] = useState('')
+  const [fullName, setFullName] = useState('')
   const [email, setEmail] = useState('')
   const [country, setCountry] = useState('')
 
@@ -21,69 +20,67 @@ function SectionBook() {
   })
 
   const navigate = useNavigate()
-  const { error, value } = formSchema.validate({ fullName: guestData, email, country })
+  const { error, value } = formSchema.validate({ fullName, email, country })
 
   const passdataToTheNextStep = event => {
     event.preventDefault()
 
     // console.log(error)
+    if (error) return <FlashMassage /> //console.log(error.details[0].message)
     console.log(value)
-    if (error) return <FlashMassege /> //console.log(error.details[0].message)
 
-    // alert('Passing data to the popup')
+    const valueAsString = JSON.stringify(value)
+    localStorage.setItem('prevData', valueAsString)
     navigate('/popup')
   }
 
   return (
-    <BookingContext.Provider value={value}>
-      {console.log(value)}
-      <section className="section-book" id="section-book">
-        <div className="row">
-          <div className="book">
-            <div className="book__form">
-              <form onSubmit={passdataToTheNextStep} className="form">
-                <div className="u-margin-bottom-small">
-                  <h2 className="heading-secondary">Start booking now</h2>
-                </div>
+    <section className="section-book" id="section-book">
+      <div className="row">
+        <div className="book">
+          <div className="book__form">
+            <form onSubmit={passdataToTheNextStep} className="form">
+              <div className="u-margin-bottom-small">
+                <h2 className="heading-secondary">Start booking now</h2>
+              </div>
 
-                <div className="form__group">
-                  <input type="text" className="form__input" placeholder="Full name" id="name" required onChange={event => setFullName(event.target.value)} />
-                  <label htmlFor="name" className="form__label">
-                    Full name
-                  </label>
-                </div>
+              <div className="form__group">
+                <input type="text" className="form__input" placeholder="Full name" id="name" required onChange={event => setFullName(event.target.value)} />
+                <label htmlFor="name" className="form__label">
+                  Full name
+                </label>
+              </div>
 
-                <div className="form__group">
-                  <input type="email" className="form__input" placeholder="Email address" id="email" required onChange={event => setEmail(event.target.value)} />
-                  <label htmlFor="email" className="form__label">
-                    Email address
-                  </label>
-                </div>
+              <div className="form__group">
+                <input type="email" className="form__input" placeholder="Email address" id="email" required onChange={event => setEmail(event.target.value)} />
+                <label htmlFor="email" className="form__label">
+                  Email address
+                </label>
+              </div>
 
-                <div className="form__group">
-                  <select className="form__input" placeholder="Where are you from?" id="country" required onChange={event => setCountry(event.target.value)}>
-                    <option value="">Where are you from?</option>
-                    {countries.map((country, index) => (
-                      <option key={index} value={country} className="paragraph">
-                        {country}
-                      </option>
-                    ))}
-                  </select>
-                  <label htmlFor="email" className="form__label">
-                    Country
-                  </label>
-                </div>
-                <div className="form__group">
-                  <button className="btn btn--green btn--animeted" onClick={passdataToTheNextStep}>
-                    Next step &rarr;
-                  </button>
-                </div>
-              </form>
-            </div>
+              <div className="form__group">
+                <select className="form__input" placeholder="Where are you from?" id="country" required onChange={event => setCountry(event.target.value)}>
+                  <option value="">Where are you from?</option>
+                  {countries.map((country, index) => (
+                    <option key={index} value={country} className="paragraph">
+                      {country}
+                    </option>
+                  ))}
+                </select>
+                <label htmlFor="email" className="form__label">
+                  Country
+                </label>
+              </div>
+              <div className="form__group">
+                <button className="btn btn--green btn--animeted" onClick={passdataToTheNextStep}>
+                  Next step &rarr;
+                </button>
+              </div>
+            </form>
           </div>
         </div>
-      </section>
-    </BookingContext.Provider>
+      </div>
+    </section>
   )
 }
 
