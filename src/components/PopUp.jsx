4fdc6cca043188bcb2bfc33dda67b3b52a78b847
Eyub-Abdi <ayubabdiy@gmail.com>
2024-09-19 23:47:58 +0300

    Fetch previous data and combine them with newly validated data

--- src/components/PopUp.jsx
+++ src/components/PopUp.jsx
@@ -1,16 +1,31 @@
-import { useState, useContext } from 'react'
+import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-// CONTEXTS
-import BookingContext from '../BookingContext'
-// import Joi from 'joi'
 
-function PopUp({ data }) {
-  const [numberOfGuests, setNumberOfGuests] = useState()
+import Joi from 'joi'
+
+function PopUp() {
+  const [numberOfVisitor, setNumberOfVisitor] = useState()
   const [arrivingDate, setArrivingDate] = useState()
-  console.log(data)
+
   const navigate = useNavigate()
-  const handleSubmit = () => {
-    // e.preventDefalut()
+  // Allow today in valid date list
+  const today = new Date()
+  today.setHours(0, 0, 0, 0)
+
+  const schema = Joi.object({
+    numberOfVisitor: Joi.number().min(1).max(1000).required().messages({ 'any.required': 'Tell us how many people are comming?', 'number.min': 'How many of you are comming', 'number.max': 'Can not register more then 1000 people at the same time.' }),
+    arrivingDate: Joi.date().min(today).required().messages({ 'any.required': 'Ops! so when will you come?', 'date.min': 'Date must be today or latter' })
+  })
+  const { error } = schema.validate({ numberOfVisitor, arrivingDate })
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    if (error) return console.log(error.details[0].message)
+    const prevData = JSON.parse(localStorage.getItem('prevData'))
+    // SUBMIT THIS DATA
+    const totalData = { ...prevData, numberOfVisitor, arrivingDate }
+    console.log(totalData)
+    localStorage.removeItem('prevData')
     // alert('Some data has been submited')
     navigate('/')
   }
@@ -19,9 +34,7 @@ function PopUp({ data }) {
     console.log('Auu')
     navigate('/')
   }
-  // const prevData = useContext(BookingContext)
-  // console.log(prevData)
-  // Handle data subimition
+
   return (
     <div className={`popup ${hidePopUp.close}`} id="popup">
       <div className="popup__content">
@@ -38,14 +51,14 @@ function PopUp({ data }) {
 
           <form onSubmit={handleSubmit} className="form">
             <div className="form__group">
-              <input type="number" className="form__input" placeholder="How many of you are comming?" defaultValue={1} id="name" required style={{ backgroundColor: '#eee', fontSize: '1.8rem' }} onChange={event => setNumberOfGuests(event.target.value)} />
+              <input type="number" className="form__input" placeholder="How many of you are comming?" id="name" style={{ backgroundColor: '#eee', fontSize: '1.8rem' }} onChange={event => setNumberOfVisitor(event.target.value)} />
               <label htmlFor="name" className="form__label">
                 How many of you
               </label>
             </div>
 
             <div className="form__group">
-              <input type="date" className="form__input" placeholder="When do we meet" id="email" style={{ backgroundColor: '#eee', fontSize: '1.8rem' }} required onChange={event => setArrivingDate(event.target.value)} />
+              <input type="date" className="form__input" placeholder="When do we meet" id="email" style={{ backgroundColor: '#eee', fontSize: '1.8rem' }} onChange={event => setArrivingDate(event.target.value)} />
               <label htmlFor="email" className="form__label">
                 Date to arrive
               </label>
