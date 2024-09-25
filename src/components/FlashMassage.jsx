16246d1f704f32f41f30061e1560ba1907187cc4
Eyub-Abdi <ayubabdiy@gmail.com>
2024-09-25 15:13:02 +0300

    Refactor Flash messages notifications

--- src/components/FlashMassage.jsx
+++ src/components/FlashMassage.jsx
@@ -1,13 +1,20 @@
 import { useToast } from '@chakra-ui/react'
+import { useEffect } from 'react'
 
-function FlashMassage() {
+// CONTEXT
+function FlashMassage({ showFlash, title, description, status }) {
   const toast = useToast()
-  return toast({
-    title: 'Account created.',
-    description: "We've created your account for you.",
-    status: 'success',
-    duration: 9000,
-    isClosable: true
-  })
+  useEffect(() => {
+    if (showFlash)
+      toast({
+        title, //: 'Account created.',
+        description, //: "We've created your account for you.",
+        status, //: 'error',
+        duration: 6000,
+        isClosable: true,
+        padding: '20px'
+      })
+  }, [showFlash, toast, title, description, status])
+  return null
 }
 export default FlashMassage
